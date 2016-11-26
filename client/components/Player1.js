aa821de0c5f18784c1a87a6044ef268e8c968e7c
Robert Ferguson <rob@rewferguson.com>
2016-11-26 14:23:12 -0600

    player ui updates

--- client/components/Player1.js
+++ client/components/Player1.js
@@ -23,20 +23,18 @@ export default class Player1 extends React.Component {
     console.log('word', this.props.word);
     const guessedLettersUpper = this.props.guessedLetters.map(letter => letter.toUpperCase());
     return (
-      <section id="player1" className="row">
-        <div className="col-sm-4" id="gallows-col">
-          <Gallows remainingGuesses={this.props.remainingGuesses} />
-        </div>
-        <div className="col-sm-8">
-          <div className="board">
-            <h1 className="board__roomName">{this.props.roomName}</h1>
-            <div className="board__playerName">Player 1:  {this.props.username}</div>
-            <div id="guessed-row" className="board__letters">
-              <GuessedLetters guessedLetters={guessedLettersUpper} />
-              <RemainingGuess remainingGuesses={this.props.remainingGuesses} />
-              <Word word={this.props.word} />
-            </div>
+      <section id="player1" className="player">
+        <Gallows remainingGuesses={this.props.remainingGuesses} />
+
+        <div className="board">
+          <h1 className="board__roomName">{this.props.roomName}</h1>
+          <div className="board__playerName">
+            <span>Player 1:</span>
+            <span>{this.props.username}</span>
           </div>
+          <RemainingGuess remainingGuesses={this.props.remainingGuesses} />
+          <GuessedLetters guessedLetters={guessedLettersUpper} />
+          <Word word={this.props.word} />
           <Alphabets
             guessedLetters={guessedLettersUpper}
             coolDown={this.props.coolDown}
